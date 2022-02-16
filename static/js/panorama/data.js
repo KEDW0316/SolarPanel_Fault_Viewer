78bdfef08147ad40f889c0b0e5bcdb774e5bad2b
unknown <dks01972@konkuk.ac.kr>
2022-02-16 22:47:20 +0900

    add panorama image

--- static/js/panorama/data.js
+++ static/js/panorama/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-_",
-      "name": "가을_건국대",
+      "id": "0-_1",
+      "name": "거제도_1",
       "levels": [
         {
           "tileSize": 256,
@@ -32,8 +32,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "1-_",
-      "name": "겨울_건국대",
+      "id": "1-_2",
+      "name": "거제도_2",
       "levels": [
         {
           "tileSize": 256,
@@ -63,8 +63,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2--1",
-      "name": "거제 1",
+      "id": "2-_",
+      "name": "건국대_가을",
       "levels": [
         {
           "tileSize": 256,
@@ -94,8 +94,39 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "3--2",
-      "name": "거제 2",
+      "id": "3-__",
+      "name": "건국대_가을_신공학관옥상",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-_",
+      "name": "건국대_겨울",
       "levels": [
         {
           "tileSize": 256,
